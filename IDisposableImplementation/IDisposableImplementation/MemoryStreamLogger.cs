dd9f0e2f0134fde068a77970e234037418ee8e05
Ivan Ivanich <dadavanja@yandex.ru>
2016-11-14 13:19:12 +0200

    "IDIsposableImplementation" task completed

---
+++ IDisposableImplementation/IDisposableImplementation/MemoryStreamLogger.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+
+namespace NetMentoring
+{
+    public class MemoryStreamLogger : IDisposable
+    {
+        private FileStream memoryStream;
+        private StreamWriter streamWriter;
+
+        public MemoryStreamLogger()
+        {
+            memoryStream = new FileStream(@"\log.txt", FileMode.OpenOrCreate);
+            memoryStream.Seek(0, SeekOrigin.End);
+            streamWriter = new StreamWriter(memoryStream);
+        }
+
+        public void Log(string message)
+        {
+            streamWriter.Write(message);
+        }
+
+        private bool disposedValue = false; 
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    streamWriter.Dispose();
+                    memoryStream.Dispose();
+                }
+
+                disposedValue = true;
+            }
+        }
+
+       
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+    }
+}
